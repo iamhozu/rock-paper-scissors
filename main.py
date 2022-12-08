976426594485483c661f1979b0e1bf4089bb9686
Hozaifa <83779542+iamhozu@users.noreply.github.com>
2022-12-08 12:39:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+play=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+if play==0:
+  print(f"you choose : {rock}")
+elif play==1:
+  print(f"you choose : {paper}") 
+else:
+  print(f"you choose : {scissors}")
+computer= random.randint(0, 1)
+if computer==0:
+  print(f"computer choose : {rock}")
+elif computer==1:
+  print(f"computer choose : {paper}") 
+elif computer==2:
+  print(f"computer choose : {scissors}")
+if computer==play:
+  print("Draw")
+elif computer==0 and play==1 or computer==1 and play==2:
+  print("You win")
+elif computer==0 and play==2 or computer==2 and play==1:
+  print("you lose")
